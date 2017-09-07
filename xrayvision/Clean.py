c837474b65d1c23f9303dbbd83768dfea75de59a
Gábor Péterffy <peterffy95@gmail.com>
2017-09-08 00:23:12 +0300

    Reimplementing Hogbom's clean - bugfixing needed!

--- xrayvision/Clean.py
+++ xrayvision/Clean.py
@@ -1,6 +1,12 @@
 """CLEAN algorithms"""
 
+from scipy.ndimage.interpolation import shift
+from scipy import signal
 import numpy as np
+import copy
+from astropy.convolution import Gaussian2DKernel
+
+from .Visibility import Visibility
 
 
 class Hogbom(object):
@@ -8,12 +14,23 @@ class Hogbom(object):
     Simplest Hogböm's CLEAN method (in case of a dirty beam with just 1s
     where there is a visibility value.
 
+    1. Perform a back projection or map_idft on the visibilities to obtain the dirty map (DM)
+    2. Perform using the same u, v locations back projection of a delta function to obtain
+       the dirty beam (DB)
+    3. Find the brightest point in the in the DM add to a point source map (position and intensity)
+    4. Subtract at the position of the bright point the DB * intensity of brightest point * gain
+    5. Go to 3 if more source or iteration threshold reached
+    6. Convolve the point source map with an idealised BEAM (small gaussian)
+    7. Add the residuals from the DM to the step above an return
+
     Parameters
     ----------
     visibility: Visibility
         The processed visibility bag
     threshold: float
         If the highest value is smaller, it stops
+    image_dimensions: tuple
+        The dimensions of the image what is obtained from the visibilities
     gain: The gain what is used for the iterations
 
     Examples
@@ -23,10 +40,30 @@ class Hogbom(object):
     -----
 
     """
-    def __init__(self, visibility, threshold, gain=0.25):
+    def __init__(self, visibility, threshold: float, image_dimensions: tuple,
+                 gain=0.25):
         self.vis = visibility
         self.thres = threshold
         self.gain = gain
+        # Check the validity of the image_dimensions input
+        if not len(image_dimensions) == 2:
+            raise ValueError("image_dimensions: incorrect tuple length! "
+                             "Example: (100x100)")
+        if image_dimensions[0] == 0 or image_dimensions[1] == 0:
+            raise ValueError("image_dimensions: 0 is not a valid size!")
+        self.dim = image_dimensions
+        self.point_source_map = np.zeros(image_dimensions)
+        # #1
+        self.dirty_map = np.zeros(self.dim)
+        self.dirty_map = self.vis.to_map(self.dirty_map)
+
+        # #2 Creating the dirty beam
+        uv_in_d = copy.deepcopy(self.vis.uv)
+        g_vis = np.ones((uv_in_d.shape[0]), dtype=complex)
+
+        db_vis = Visibility(uv_in_d, g_vis)
+        self.dirty_beam = np.zeros(self.dim)
+        self.dirty_beam = db_vis.to_map(self.dirty_beam)
 
     def iterate(self, gain=False):
         """
@@ -45,20 +82,55 @@ class Hogbom(object):
         -----
 
         """
+        # #3
         max_intesity = 0.0
-        pos = 0
+        pos = (0, 0)
         summarized = 0.0
-        for i, j in enumerate(np.absolute(self.vis.vis)):
-            if j > max_intesity:
-                max_intesity = j
-                pos = i
-                summarized += j
-        abs_avg_value = summarized / float(self.vis.vis[pos])
+        for i in range(self.dirty_map.shape[0]):
+            for j in range(self.dirty_map.shape[1]):
+                intensity = self.dirty_map[i, j]
+                if intensity > max_intesity:
+                    max_intesity = intensity
+                    pos = (i, j)
+                    summarized += intensity
+        # Checking if we reached the threshold or not
+        abs_avg_value = summarized / self.dirty_map[pos[0], pos[1]]
         if abs_avg_value < self.thres:
             return True
+        # If gain is not set, using the one that was give during init
         if not gain:
             gain = self.gain
-        clean_vis = complex(gain*self.vis.vis[pos])
-        for i in range(self.vis.vis.shape[0]):
-            self.vis.vis[i] -= clean_vis
+
+        # Updating the point source map
+        self.point_source_map[pos[0], pos[1]] += max_intesity
+        # #4
+        # Centering the the dirty_beam on the position of the max intensity
+        centered_dirty_beam = shift(self.dirty_beam, (pos[0]-int(self.dim[0]/2),
+                                                      pos[1]-int(self.dim[1]/2)))
+        self.dirty_map = np.substract(self.dirty_map,
+                                      centered_dirty_beam * max_intesity * gain)
         return False
+
+    def finish(self, stddev: float):
+        """
+        Returns with the cleaned map
+
+        Parameters
+        ----------
+        stddev: float
+            The standard deviation of the convolved Gaussian
+
+        Examples
+        --------
+
+        Notes
+        -----
+
+        """
+        # #6
+        gaussian_map = Gaussian2DKernel(stddev=stddev, x_size=self.dim[0],
+                                        y_size=self.dim[1])
+        result = signal.convolve2d(self.point_source_visibility, gaussian_map)
+
+        # 7
+        return np.add(result, self.dirty_map)
--- xrayvision/tests/test_clean.py
+++ xrayvision/tests/test_clean.py
@@ -1,28 +1,47 @@
+"""
 import numpy as np
 import pytest
+from astropy.convolution import Gaussian2DKernel
+from scipy.ndimage.interpolation import shift
+from scipy import signal
 
 from ..Clean import Hogbom
 from ..Visibility import Visibility
 
 
 class TestClean(object):
-    # Typical map sizes even, odd
-    @pytest.mark.parametrize("", [()])
-    def test_hogbom_intensity_change(self):
-        # Create a visibility bag
-        u = [5, 7, 15, 37, 45, 57, 60]
-        v = [1, 3, 7, 27, 37, 1, 57]
-        uv_in = np.array([u, v]).reshape(2, len(u))
-        vis_in = [5+7j, 1+2j, 10-5j, -6+1j, 7j, -7-7j, 5j]
+    # Typical map sizes even, odd and two point sources
+    @pytest.mark.parametrize("N,M,pos1,pos2", [(65, 65, (15, 30), (40, 32)),
+                                               (64, 64, (15, 30), (40, 32))])
+    def test_hogbom_intensity_change(self, N, M, pos1, pos2):
+        # Creating a "clean" map as a base with 2 point sources
+        gaussian = Gaussian2DKernel(stddev=3, x_size=N, y_size=M).array
+        clean_map = np.add(shift(gaussian, (pos1[0]-int(N/2),
+                                            pos1[1]-int(M/2))),
+                           shift(gaussian, (pos2[0]-int(N/2),
+                                            pos2[1]-int(M/2))))
+        psf = np.array([[0., 0., 0., 0., 0.5, 0., 0., 0., 0.],
+                        [0., 0., 0., 0., 0.5, 0., 0., 0., 0.],
+                        [0., 0., 0., 0., 0.5, 0., 0., 0., 0.],
+                        [0., 0., 0., 0.5, 0., 0.5, 0., 0., 0.],
+                        [0.5, 0.5, 0., 0., 1., 0., 0., 0.5, 0.5],
+                        [0., 0., 0., 0.5, 0., 0.5, 0., 0., 0.],
+                        [0., 0., 0., 0., 0.5, 0., 0., 0., 0.],
+                        [0., 0., 0., 0., 0.5, 0., 0., 0., 0.],
+                        [0., 0., 0., 0., 0.5, 0., 0., 0., 0.]])
+        dirty_map = signal.convolve2d(clean_map, psf)
+        print(dirty_map)
 
-        # Test sanity check
-        assert len(u) == len(v) and len(vis_in) == len(v)
+        u, v = np.meshgrid(np.arange(M), np.arange(M))
+        uv_in = np.array([u, v]).reshape(2, N * M)
+        vis_in = np.zeros(N * M, dtype=complex)
 
         vis = Visibility(uv_in, vis_in)
-
-        clean = Hogbom(vis, 0.01)
-        max_abs = np.max(np.abs(vis_in))
-        clean.iterate()
-        max_abs_after_clean = np.max(np.abs(clean.vis.vis))
-        # Clean should "lower the ceiling" with every iteration
-        assert max_abs_after_clean < max_abs
+        vis.from_map(dirty_map)
+        clean = Hogbom(vis, 1., (N, M))
+        while not clean.iterate():
+            pass
+        final_image = clean.finish(3)
+        print(final_image)
+        assert np.array_equal(final_image, clean_map)
+"""
