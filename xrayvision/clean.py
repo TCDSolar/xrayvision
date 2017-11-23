a81ccf6548f047938b00d3ada744b46c5e3210c8
Shane Maloney <shane.maloney@tcd.ie>
2017-11-23 16:04:40 +0000

    Multiscale clean first implementation

--- xrayvision/clean.py
+++ xrayvision/clean.py
@@ -10,7 +10,7 @@ from astropy.convolution import Gaussian2DKernel
 from scipy import signal
 from scipy.ndimage.interpolation import shift
 
-__all__ = ['clean']
+__all__ = ['clean', 'ms_clean']
 
 
 def clean(dirty_map, dirty_beam, clean_beam_width=4.0, gain=0.1, thres=0.01, niter=1000):
@@ -50,10 +50,10 @@ def clean(dirty_map, dirty_beam, clean_beam_width=4.0, gain=0.1, thres=0.01, nit
     for _i in range(niter):
         # Find max in dirty map and save to point source
         mx, my = np.unravel_index(dirty_map.argmax(), dirty_map.shape)
-        Imax = dirty_map[mx, my]
-        model[mx, my] += gain*Imax
+        imax = dirty_map[mx, my]
+        model[mx, my] += gain*imax
 
-        comp = Imax * gain * shift(dirty_beam, (mx - beam_center[0], my - beam_center[1]), order=0)
+        comp = imax * gain * shift(dirty_beam, (mx - beam_center[0], my - beam_center[1]), order=0)
 
         dirty_map = np.subtract(dirty_map, comp)
 
@@ -70,3 +70,322 @@ def clean(dirty_map, dirty_beam, clean_beam_width=4.0, gain=0.1, thres=0.01, nit
         dirty_map = dirty_map / clean_beam.sum()
 
     return model + dirty_map
+
+
+def ms_clean(dirty_map, dirty_beam, scales=None,
+             clean_beam_width=4.0, gain=0.1, thres=0.01, niter=1000):
+    r"""
+
+    Parameters
+    ----------
+    dirty_map
+    dirty_beam
+    scales
+    clean_beam_width
+    gain
+    thres
+    niter
+
+    Returns
+    -------
+
+    Notes
+    -----
+    This is an implementation of the multiscale clean algorithm as outlined in [1] adapted for x-ray
+    Fourier observations. In particular the choice of scale is not related to resolution
+    :math:`\lambda/D` but is chosen
+
+    References
+    ----------
+    .. [1] Cornwell, T. J., "Multiscale CLEAN Deconvolution of Radio Synthesis Images", IEEE Journal
+    of Selected Topics in Signal Processing, vol 2, p793-801
+
+    """
+    # Compute the number of dyadic scales, their sizes and scale biases
+    number_of_scales = np.floor(np.log2(min(dirty_map.shape))).astype(int)
+    scale_sizes = 2**np.arange(number_of_scales)
+    scale_biases = 1 - 0.6 * scale_sizes / scale_sizes.max()
+
+    if scales:
+        scales = np.array(scales)
+        number_of_scales = len(scales)
+        scale_sizes = scales
+        scale_biases = [1.0]
+
+    model = np.zeros(dirty_map.shape)
+
+    # Pre-compute scales, residual maps and dirty beams at each scale and dirty beam cross terms
+    scales = np.zeros((dirty_map.shape[0], dirty_map.shape[1], number_of_scales))
+    scaled_residuals = np.zeros((dirty_map.shape[0], dirty_map.shape[1], number_of_scales))
+    scaled_dirty_beams = np.zeros((dirty_beam.shape[0], dirty_beam.shape[1], number_of_scales))
+    max_scaled_dirty_beams = np.zeros(number_of_scales)
+    cross_terms = {}
+
+    for i, scale in enumerate(scale_sizes):
+        scales[:, :, i] = component(scale=scale, shape=dirty_map.shape)
+        scaled_residuals[:, :, i] = signal.convolve(dirty_map, scales[:, :, i], mode='same')
+        scaled_dirty_beams[:, :, i] = signal.convolve(dirty_beam, scales[:, :, i], mode='same')
+        max_scaled_dirty_beams[i] = scaled_dirty_beams[:, :, i].max()
+        for j in range(i, number_of_scales):
+            cross_terms[(i, j)] = signal.convolve(
+                signal.convolve(dirty_beam, scales[:, :, i], mode='same'),
+                scales[:, :, j], mode='same')
+
+    # Clean loop
+    for i in range(niter):
+        print(f'Clean loop {i}')
+        # For each scale find the strength and location of max residual
+        # Chose scale with has maximum strength
+        max_index = np.argmax(scaled_residuals)
+        max_x, max_y, max_scale = np.unravel_index(max_index, scaled_residuals.shape)
+
+        strength = scaled_residuals[max_x, max_y, max_scale]
+
+        # Adjust for the max of scaled beam
+        strength = strength / max_scaled_dirty_beams[max_scale]
+
+        # Loop gain and scale dependent bias
+        strength = strength * scale_biases[max_scale] * gain
+
+        beam_center = [(scaled_dirty_beams[:, :, max_scale].shape[0] - 1) / 2.0,
+                       (scaled_dirty_beams[:, :, max_scale].shape[1] - 1) / 2.0]
+
+        comp = strength * shift(scales[:, :, max_scale],
+                                (max_x - beam_center[0], max_y - beam_center[1]), order=0)
+
+        # Add this component to current model
+        model = np.add(model, comp)
+
+        # Update all images using precomputed terms
+        # Straight forward for current scale
+
+        # scaled_residuals[:, :, max_scale] = np.subtract(scaled_residuals[:, :, max_scale], comp)
+
+        # Need to use precomputed terms for effect on other scales
+        for j, _ in enumerate(scale_sizes):
+            if j > max_scale:
+                cross_term = cross_terms[(max_scale, j)]
+            else:
+                cross_term = cross_terms[(j, max_scale)]
+
+            comp = strength * shift(cross_term,
+                                    (max_x - beam_center[0], max_y - beam_center[1]), order=0)
+
+            scaled_residuals[:, :, j] = np.subtract(scaled_residuals[:, :, j], comp)
+
+        # End max(res(a)) or niter
+        if np.fabs(scaled_residuals[:, :, max_scale]).max() <= thres:
+            print("Threshold reached")
+            break
+    else:
+        print("Max iterations reached")
+
+    # Convolve model with clean beam  B_G * I^M
+    if clean_beam_width != 0.0:
+        clean_beam = Gaussian2DKernel(stddev=clean_beam_width, x_size=dirty_beam.shape[1],
+                                      y_size=dirty_beam.shape[0]).array
+
+        model = signal.convolve2d(model, clean_beam, mode='same')  # noqa
+
+    # Add residuals B_G * I^M + I^R
+    return model + scaled_residuals.sum(axis=2)
+
+
+def radial_prolate_sphereoidal(nu):
+    r"""
+    Calculate prolate spheroidal wave function approximation.
+
+    Parameters
+    ----------
+    nu : `float`
+        The radial value to evaluate the function at
+
+    Returns
+    -------
+    `float`
+        The amplitude of the the prolate spheroid function at `nu`
+
+    Notes
+    -----
+    Note this is a direct translation of the on the implementation the CASA code reference by [1] \
+    and can be found here Link_
+
+    .. _Link: https://github.com/casacore/casacore/blob/f4dc1c36287c766796ce3375cebdfc8af797a388/lattices/LatticeMath/LatticeCleaner.tcc#L956
+
+    """
+    if nu <= 0:
+        return 1.0
+    elif nu >= 1.0:
+        return 0.0
+    else:
+        n_p = 5
+        n_q = 3
+
+        p = np.zeros((n_p, 2))
+        q = np.zeros((n_q, 2))
+
+        p[0, 0] = 8.203343e-2
+        p[1, 0] = -3.644705e-1
+        p[2, 0] = 6.278660e-1
+        p[3, 0] = -5.335581e-1
+        p[4, 0] = 2.312756e-1
+        p[0, 1] = 4.028559e-3
+        p[1, 1] = -3.697768e-2
+        p[2, 1] = 1.021332e-1
+        p[3, 1] = -1.201436e-1
+        p[4, 1] = 6.412774e-2
+
+        q[0, 0] = 1.0000000e0
+        q[1, 0] = 8.212018e-1
+        q[2, 0] = 2.078043e-1
+        q[0, 1] = 1.0000000e0
+        q[1, 1] = 9.599102e-1
+        q[2, 1] = 2.918724e-1
+
+        part = 0
+        nuend = 0.0
+
+        if nu >= 0.0 and nu < 0.75:
+            part = 0
+            nuend = 0.75
+        elif nu >= 0.75 and nu <= 1.00:
+            part = 1
+            nuend = 1.0
+
+        top = p[0, part]
+        delnusq = np.power(nu, 2.0) - np.power(nuend, 2.0)
+
+        for k in range(1, n_p):
+            top += p[k, part] * np.power(delnusq, k)
+
+        bot = q[0, part]
+        for k in range(1, n_q):
+            bot += q[k, part] * np.power(delnusq, k)
+
+        if bot != 0.0:
+            return top/bot
+        else:
+            return 0
+
+
+def vec_radial_prolate_sphereoidal(nu):
+    r"""
+    Calculate prolate spheroidal wave function approximation.
+
+    Parameters
+    ----------
+    nu : `float` array
+        The radial value to evaluate the function at
+
+    Returns
+    -------
+    `float`
+        The amplitude of the the prolate spheroid function at `nu`
+
+    Notes
+    -----
+    Note this is based on the implementation the CASA code reference by [1] and can be found here
+    Link_
+
+    .. _Link: https://github.com/casacore/casacore/blob/f4dc1c36287c766796ce3375cebdfc8af797a388/lattices/LatticeMath/LatticeCleaner.tcc#L956
+
+    """
+    nu = np.array(nu)
+
+    n_p = 5
+    n_q = 3
+
+    p = np.zeros((n_p, 2))
+    q = np.zeros((n_q, 2))
+
+    p[0, 0] = 8.203343e-2
+    p[1, 0] = -3.644705e-1
+    p[2, 0] = 6.278660e-1
+    p[3, 0] = -5.335581e-1
+    p[4, 0] = 2.312756e-1
+    p[0, 1] = 4.028559e-3
+    p[1, 1] = -3.697768e-2
+    p[2, 1] = 1.021332e-1
+    p[3, 1] = -1.201436e-1
+    p[4, 1] = 6.412774e-2
+
+    q[0, 0] = 1.0000000e0
+    q[1, 0] = 8.212018e-1
+    q[2, 0] = 2.078043e-1
+    q[0, 1] = 1.0000000e0
+    q[1, 1] = 9.599102e-1
+    q[2, 1] = 2.918724e-1
+
+    lower = np.where((nu >= 0.0) & (nu < 0.75))  # part = 0, nuend = 0.75
+    upper = np.where((nu >= 0.75) & (nu <= 1.00))  # part = 1, nuend = 1.0
+
+    delnusq = np.zeros_like(nu)
+    delnusq[lower] = np.power(nu[lower], 2.0) - np.power(0.75, 2.0)
+    delnusq[upper] = np.power(nu[upper], 2.0) - np.power(1.00, 2.0)
+
+    top = np.zeros_like(nu, dtype=float)
+    top[lower] = p[0, 0]
+    top[upper] = p[0, 1]
+
+    k = np.arange(1, n_p)
+    top[lower] += np.sum(p[k, 0, np.newaxis] * np.power(delnusq[lower], k[..., np.newaxis]), axis=0)
+    top[upper] += np.sum(p[k, 1, np.newaxis] * np.power(delnusq[upper], k[..., np.newaxis]), axis=0)
+
+    bot = np.zeros_like(nu, dtype=float)
+    bot[lower] = q[0, 0]
+    bot[upper] = q[0, 1]
+
+    j = np.arange(1, n_q)
+    bot[lower] += np.sum(q[j, 0, np.newaxis] * np.power(delnusq[lower], j[..., np.newaxis]), axis=0)
+    bot[upper] += np.sum(q[j, 1, np.newaxis] * np.power(delnusq[upper], j[..., np.newaxis]), axis=0)
+
+    out = np.where(bot != 0.0, top/bot, 0.0)
+
+    out = np.where(nu <= 0, 1.0, out)
+    out = np.where(nu >= 1, 0.0, out)
+
+    return out
+
+
+def component(scale, shape):
+    r"""
+
+    Parameters
+    ----------
+    scale
+
+    Returns
+    -------
+
+    """
+    # if scale == 0.0:
+    #     out = np.zeros((3, 3))
+    #     out[1,1] = 1.0
+    #     return out
+    # elif scale % 2 == 0:  # Even so keep output even
+    #     shape = np.array((2 * scale + 2, 2 * scale + 2), dtype=int)
+    # else:  # Odd so keep odd
+    #     shape = np.array((2 * scale + 1, 2 * scale + 1), dtype=int)
+
+    refx, refy = (np.array(shape) - 1) / 2.0
+
+    if scale == 0.0:
+        wave_amp = np.zeros(shape)
+        wave_amp[int(refx), int(refy)] = 1
+        return wave_amp
+
+    xy = np.mgrid[0:shape[0]:1, 0:shape[1]:1]
+    radii_squared = ((xy[0, :, :] - refx) / scale)**2 + ((xy[1, :, :] - refy) / scale)**2
+
+    rad_zeros_indices = radii_squared <= 0.0
+    amp_zero_indices = radii_squared >= 1.0
+
+    wave_amp = vec_radial_prolate_sphereoidal(np.sqrt(radii_squared.reshape(radii_squared.size)))
+    wave_amp = wave_amp.reshape(shape)
+    wave_amp[rad_zeros_indices] = vec_radial_prolate_sphereoidal([0])[0]
+
+    wave_amp = wave_amp * (1 - radii_squared)
+
+    wave_amp[amp_zero_indices] = 0.0
+
+    return wave_amp
--- xrayvision/tests/test_clean.py
+++ xrayvision/tests/test_clean.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy import signal
 
-from ..clean import clean
+from ..clean import clean, ms_clean, component, radial_prolate_sphereoidal,\
+    vec_radial_prolate_sphereoidal
 
 
 def test_clean():
@@ -26,7 +27,72 @@ def test_clean():
     # Within threshold default threshold
     assert np.allclose(clean_map, out_map, atol=0.01)
     temp = np.argsort(out_map.ravel())[-2:]
-    max_loccations = np.dstack(np.unravel_index(temp, out_map.shape))
+    max_locations = np.dstack(np.unravel_index(temp, out_map.shape))
     # Position of max equal those set above
-    assert max_loccations[0][1].tolist() == pos1
-    assert max_loccations[0][0].tolist() == pos2
+    assert max_locations[0][1].tolist() == pos1
+    assert max_locations[0][0].tolist() == pos2
+
+
+def test_component():
+    comp = np.zeros((3, 3))
+    comp[1,1] = 1.0
+
+    res = component(scale=0, shape=(3, 3))
+    assert np.array_equal(res, comp)
+
+    res = component(scale=1, shape=(3, 3))
+    assert np.array_equal(res, comp)
+
+    res = component(scale=2, shape=(6,6))
+    assert np.all(res[0, :] == 0.0)
+    assert np.all(res[:, 0] == 0.0)
+    assert np.all(res[2:4, 2:4] == res.max())
+
+    res = component(scale=3, shape=(7,7))
+    assert np.all(res[0, :] == 0.0)
+    assert np.all(res[:, 0] == 0.0)
+    assert res[3, 3] == 1
+
+
+def test_radial_prolate_sphereoidal():
+    amps = [radial_prolate_sphereoidal(r) for r in [-1.0, 0.0, 0.5, 1.0, 2.0]]
+    assert amps[0] == 1.0
+    assert amps[1] == 1.0
+    assert amps[2] == 0.36106538453111797
+    assert amps[3] == 0.0
+    assert amps[4] == 0.0
+
+
+def test_vec_radial_prolate_sphereoidal():
+    radii = np.linspace(-0.5, 1.5, 1000)
+    amps1 = [radial_prolate_sphereoidal(r) for r in radii ]
+    amps2 = vec_radial_prolate_sphereoidal(radii)
+    assert np.allclose(amps1, amps2)
+
+
+def test_ms_clean():
+    n = m = 65
+    pos1 = [15, 30]
+    pos2 = [40, 32]
+
+    clean_map = np.zeros((n, m))
+    clean_map[pos1[0], pos1[1]] = 10.
+    clean_map[pos2[0], pos2[1]] = 7.
+
+    dirty_beam = np.zeros((n, m))
+    dirty_beam[(n-1)//4:(n-1)//4 + (n-1)//2, (m-1)//2] = 0.75
+    dirty_beam[(n-1)//2, (m-1)//4:(m-1)//4 + (m-1)//2, ] = 0.75
+    dirty_beam[(n-1)//2, (m-1)//2] = 1.0
+
+    dirty_map = signal.convolve2d(clean_map, dirty_beam, mode='same')
+
+    # Disable convolution of model with gaussian for testing
+    out_map = ms_clean(dirty_map, dirty_beam, scales=[0], clean_beam_width=0.0)
+
+    # Within threshold default threshold
+    assert np.allclose(clean_map, out_map, atol=0.01)
+    temp = np.argsort(out_map.ravel())[-2:]
+    max_locations = np.dstack(np.unravel_index(temp, out_map.shape))
+    # Position of max equal those set above
+    assert max_locations[0][1].tolist() == pos1
+    assert max_locations[0][0].tolist() == pos2
