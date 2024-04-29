aed809b47592b2c01e549ea3da32b3c9721817c4
annavolp <volparaanna@gmail.com>
2024-04-29 18:23:02 +0200

    define first draft definition of visibility class API

--- xrayvision/visibility.py
+++ xrayvision/visibility.py
@@ -5,22 +5,101 @@ This contains classes to hold general visibilities and specialised classes hold 
 certain spacecraft or instruments
 """
 
+import abc
 import astropy.units as apu
 import numpy as np
+import astropy.coordinates
+import astropy.units as u
+
 
 __all__ = ['Visibility']
 
+class VisibilityABC:
+    @property
+    @abc.abstractmethod
+    def amplitude(self) -> np.ndarray:
+        """
+        Amplitudes of the visibilities.
+        """
+    
+    @property
+    @abc.abstractmethod   
+    def phase(self) -> np.ndarray:
+        """
+        Phases of the visibilities.
+        """
+    
+    @property
+    @abc.abstractmethod   
+    def center(self) -> astropy.coordinates.SkyCoord:
+        """
+        Center of the image described by the visibilities.
+        """
 
-class BaseVisibility:
-    r"""
-    Base visibility containing bare essential fields, u, v, and complex vis
-    """
-    @apu.quantity_input(u=1/apu.arcsec, v=1/apu.arcsec, center=apu.arcsec)
-    def __int__(self, u, v, vis, center=(0, 0)*apu.arcsec):
-        self.u = u
-        self.v = v
-        self.vis = vis
-        self.center = center
+    @property
+    @abc.abstractmethod   
+    def observer_coordinate(self) -> astropy.coordinates.SkyCoord:
+        """
+        Location of the observer.
+        """
+
+    @property
+    @abc.abstractmethod   
+    def energy_range(self) -> Iterable[u.Quantity]:
+        """
+        Energy range over which the visibilities are computed.
+        """
+
+    @property
+    @abc.abstractmethod   
+    def date(self) -> astropy.time.Time:
+        """
+        Centre time over which the visibilities are computed.
+        """
+
+    @property
+    @abc.abstractmethod   
+    def integration_time(self) -> u.Quantity:
+        """
+        Time duration over which the visibilities are computed.
+        """
+
+    @property
+    @abc.abstractmethod   
+    def u(self) -> np.ndarray:
+        """
+        u-coordinate on the complex plane of the visibilities.
+        """
+
+    @property
+    @abc.abstractmethod   
+    def v(self) -> np.ndarray:
+        """
+        v-coordinate on the complex plane of the visibilities.
+        """
+
+    @property
+    @abc.abstractmethod   
+    def vis(self) -> np.ndarray:
+        """
+        Complex numbers representing the visibilities.
+        """
+
+    @property
+    @abc.abstractmethod   
+    def keys(self) -> Iterable[str]:
+        """
+        Names for each visibility. 
+
+        Must be same length as self.vis.
+        """
+
+    @property
+    @abc.abstractmethod   
+    def uncertaintly(self) -> np.ndarray:
+        """
+        uncertainties on visibilities values. 
+        """
 
 
 class Visibility:
