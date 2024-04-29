689674cc0a64b0e1fa3d7dd8d52eae80ddf6f6f6
annavolp <volparaanna@gmail.com>
2024-04-29 19:54:14 +0200

    update Visibilities class

--- xrayvision/visibility.py
+++ xrayvision/visibility.py
@@ -6,9 +6,8 @@ certain spacecraft or instruments
 """
 
 import abc
-import astropy.units as apu
 import numpy as np
-import astropy.coordinates
+from astropy.coordinates import SkyCoord
 import astropy.units as u
 
 
@@ -31,14 +30,14 @@ class VisibilitiesABC:
     
     @property
     @abc.abstractmethod   
-    def center(self) -> astropy.coordinates.SkyCoord:
+    def center(self) -> SkyCoord:
         """
         Center of the image described by the visibilities.
         """
 
     @property
     @abc.abstractmethod   
-    def observer_coordinate(self) -> astropy.coordinates.SkyCoord:
+    def observer_coordinate(self) -> SkyCoord:
         """
         Location of the observer.
         """
@@ -109,7 +108,7 @@ class VisibilitiesABC:
         """
 
 
-class Visibility:
+class Visibilities:
     r"""
     Hold a set of related visibilities and information.
 
@@ -125,8 +124,8 @@ class Visibility:
         The x, y offset of phase center
 
     """
-    @apu.quantity_input(uv=1/apu.arcsec, offset=apu.arcsec, center=apu.arcsec, pixel_size=apu.arcsec)
-    def __init__(self, vis, *, u, v, offset=(0., 0.) * apu.arcsec, center=(0., 0.) * apu.arcsec):
+    @apu.quantity_input(u=1/u.arcsec, v=1/u.arcsec, energy_range=u.keV, center=apu.arcsec)
+    def __init__(self, visibilities, *, u, v, names, energy_range, date_range=None, center=None, observer_coordinate=None, uncertainty=None):
         r"""
         Initialise a new Visibility object.
 
@@ -141,11 +140,72 @@ class Visibility:
         center :
             Phase centre
         """
-        self.u = u
-        self.v = v
-        self.vis = vis
-        self.center = center
-        self.offset = offset
+        self._visibilities = visibilities
+        nvis = len(self._visibilities)
+        if len(u) != nvis: 
+            raise ValueError('u must be the same length as visibilities.')
+        self._u = u
+        if len(v) != nvis: 
+            raise ValueError('v must be the same length as visibilities.')
+        self._v = v
+        if len(names) != nvis: 
+            raise ValueError('names must be the same length as visibilities.')
+        if not (isinstance(name, str) for name in names).all(): 
+            raise TypeError('names must all be strings.')
+        self._names = names
+        if len(energy_range) != 2: 
+            raise ValueError('energy range must be length 2.')
+        self._energy_range = energy_range
+        if center is not None and (not isinstance(center, SkyCoord) or not name.isscalar): 
+            raise ValueError('center must be a scalar SkyCoord.')           
+        self._center = center
+        if date_range is not None and (not isinstance(date_range, astropy.time.Time) or len(date_range) != 2): 
+            raise ValueError('date_range must be a length 2 astropy time object.')   
+        self._date_range = date_range
+        if observer_coordinate is not None and (not isinstance(observer_coordinate, SkyCoord) or not observer_coordinate.isscalar): 
+            raise ValueError('observer_coordinate must be a scalar SkyCoord.')  
+        self._observer_coordinate = observer_coordinate
+        if uncertainty is not None and (len(uncertainty) != nvis): 
+            raise ValueError('uncertainty must be the same length as visibilities.')
+        self._uncertainty = uncertainty
+
+    @property
+    def visibilities(self): 
+        return self._visibilities
+    
+    @property
+    def u(self): 
+        return self._u
+    
+    @property
+    def v(self): 
+        return self._v
+    
+    @property
+    def names(self): 
+        return self._names
+     
+    @property
+    def energy_range(self): 
+        return self._energy_range 
+       
+    @property
+    def center(self): 
+        return self._center 
+             
+    @property
+    def date_range(self): 
+        return self._date_range 
+             
+    @property
+    def observer_coordinate(self): 
+        return self._observer_coordinate  
+             
+    @property
+    def uncertainty(self): 
+        return self._uncertainty      
+
+
 
     def __repr__(self):
         r"""
