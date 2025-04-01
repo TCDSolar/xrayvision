22ca32b3916459a49678992aeb07490f43546ce0
Shane Maloney <shane.maloney@dias.ie>
2025-04-01 15:57:25 +0100

    Maint: Update project configuration
    * Move inforamtion from setup.cfg to project.toml, .isort.cfg, pytest.ini etc and remove setup.cfg and slim down setup.py
    * Update build and version settings
    * Add zenodo.json
    * Add .mailmap

--- xrayvision/__init__.py
+++ xrayvision/__init__.py
@@ -1,7 +1,3 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-try:
-    from xrayvision.version import __version__  # type: ignore
-except ImportError:
-    __version__ = "unknown"
-__all__: list[str] = []
+from .version import version as __version__
